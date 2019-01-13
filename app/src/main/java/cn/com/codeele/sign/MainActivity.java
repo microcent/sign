f2767f89d4914f3baae6785776e724658af44ebe
microcent <abc123456>
2019-01-13 22:58:08 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/codeele/sign/MainActivity.java
@@ -0,0 +1,75 @@
+package cn.com.codeele.sign;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Banner banner;
+    private ArrayList<String> images;
+    private ArrayList<String> titles;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hideBottomUIMenu();
+        setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView(){
+        images = new ArrayList<>();
+        titles = new ArrayList<>();
+        images.add("http:////codeele.com/uploads/ad_1.jpg");
+        images.add("http:////codeele.com/uploads/ad_2.jpg");
+        images.add("http:////codeele.com/uploads/ad_3.jpg");
+        titles.add("广告标题1-VIDEO");
+        titles.add("广告标题2-IMAGE");
+        titles.add("广告标题3-IMAGE");
+        banner = (Banner) findViewById(R.id.banner);
+        //设置banner样式
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);
+        //设置图片加载器
+        banner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        banner.setImages(images);
+        //设置banner动画效果
+        banner.setBannerAnimation(Transformer.DepthPage);
+        //设置标题集合（当banner样式有显示title时）
+        banner.setBannerTitles(titles);
+        //设置自动轮播，默认为true
+        banner.isAutoPlay(true);
+        //设置轮播时间
+        banner.setDelayTime(3000);
+        //设置指示器位置（当banner模式中有指示器时）
+        banner.setIndicatorGravity(BannerConfig.CENTER);
+        //banner设置方法全部调用完毕时最后调用
+        banner.start();
+    }
+
+    //隐藏虚拟按键
+    protected void hideBottomUIMenu() {
+        //隐藏虚拟按键，并且全屏
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+            View v = this.getWindow().getDecorView();
+            v.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //for new api versions.
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
+    }
+
+}
---
+++ app/src/main/java/cn/com/codeele/sign/SignApplication.java
@@ -0,0 +1,11 @@
+package cn.com.codeele.sign;
+
+import android.app.Application;
+
+public class SignApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+
